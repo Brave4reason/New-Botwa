404c242f62c5ba8e41070602e2dbc5f34f6be039
Brave4reason <126932235+Brave4reason@users.noreply.github.com>
2023-10-28 20:56:18 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -2,16 +2,16 @@ import os
 
 class Config(object):
   API_ID = int(os.environ.get("API_ID", "25435105"))
-  API_HASH = os.environ.get("API_HASH", "011126265844f2d7cc7dc1a024f6bc78")
-  BOT_TOKEN = os.environ.get("BOT_TOKEN", "6734734174:AAERH1lMzFq2dgAi8V-Fp0WLU18_yl4OMCI")
-  BOT_USERNAME = os.environ.get("BOT_USERNAME", "fileeestorebot")
-  DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1002053044713"))
-  SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "ez4short.com")
-  SHORTLINK_API = os.environ.get('SHORTLINK_API', "ade16f386d69aa3d86d6320b5b2c906996a8d2a6")
-  BOT_OWNER = int(os.environ.get("BOT_OWNER", "6459102722"))
-  DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://SINGER:SINGER@cluster0.1dt9hoe.mongodb.net/?retryWrites=true&w=majority")
-  UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1001963382900")
-  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001956959333"))
+  API_HASH = os.environ.get("API_HASH", "5aa2f97407dbd5a32c9668a5e3a926ba")
+  BOT_TOKEN = os.environ.get("BOT_TOKEN", "6853611135:AAGKDfpA157zr4LwHYleQPG442qSAmLBpRQI")
+  BOT_USERNAME = os.environ.get("BOT_USERNAME", "MangaTalesbot")
+  DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1002087845070"))
+  SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "")
+  SHORTLINK_API = os.environ.get('SHORTLINK_API', "")
+  BOT_OWNER = int(os.environ.get("BOT_OWNER", "1179962433"))
+  DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://bravebrowsersurface:0kvqPevUYtGifz3J@cluster0.jbx8dbw.mongodb.net/NewEra")
+  UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1001362178944")
+  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
   BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
   FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
   BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
@@ -24,12 +24,12 @@ class Config(object):
 
 ▶ ᴍʏ ɴᴀᴍᴇ : ꜰɪʟᴇ ꜱᴛᴏʀᴇ
 ▶ ꜱᴇʀᴠᴇʀ : ʜᴇʀᴏᴋᴜ
-▶ ᴏᴡɴᴇʀ : [ᴋʜᴀɴᴅᴜᴅᴏɴ](t.me/khandudon302)
+▶ ᴏᴡɴᴇʀ : [ᴋʜᴀɴᴅᴜᴅᴏɴ](t.me/Xbhau)
 
 🌿 ɴᴇᴠᴇʀꜰᴏʟᴅ ɴᴇᴠᴇʀ ʙᴀᴄᴋᴅᴏᴡɴ 🌿
 """
   ABOUT_DEV_TEXT = f"""
-ᴍʏ ᴅᴇᴠ: [➡](https://telegram.me/badal6667rai)
+ᴍʏ ᴅᴇᴠ: [➡](https://telegram.me/Xbhau)
  
  I am Super noob Please Support My Hard Work. 🐸
 """
