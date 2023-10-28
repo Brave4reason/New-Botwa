6b12b887df57ec691fe35136ae99c5d208c78921
Brave4reason <126932235+Brave4reason@users.noreply.github.com>
2023-10-28 21:22:19 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -11,7 +11,7 @@ class Config(object):
   BOT_OWNER = int(os.environ.get("BOT_OWNER", "1179962433"))
   DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://bravebrowsersurface:0kvqPevUYtGifz3J@cluster0.jbx8dbw.mongodb.net/NewEra")
   UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1001362178944")
-  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002056372468"))
   BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
   FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
   BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
